915f2cd1fd72b5433855c88e29915f515d7a60cd
sunday00 <kind65505256@gmail.com>
2020-07-30 00:35:46 +0900

    frontend build

--- src/main/java/lec/spring/studygroupclone/config/SecurityConfig.java
+++ src/main/java/lec/spring/studygroupclone/config/SecurityConfig.java
@@ -28,7 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
