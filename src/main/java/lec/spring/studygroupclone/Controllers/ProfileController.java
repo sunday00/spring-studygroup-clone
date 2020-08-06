432fb3d5c7c1dc44e2c81d656acbd1472552526e
sunday00 <kind65505256@gmail.com>
2020-08-06 11:43:15 +0900

    fixing for big image

--- src/main/java/lec/spring/studygroupclone/Controllers/ProfileController.java
+++ src/main/java/lec/spring/studygroupclone/Controllers/ProfileController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
